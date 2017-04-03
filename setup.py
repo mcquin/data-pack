efe72bf29d5c40fc386e778b398ad1a26913071d
mcquin <mcquin@broadinstitute.org>
2017-04-02 21:03:33 -0400

    use setuptools. basic test framework.

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
-from distutils.core import setup
+from setuptools import setup
+
 setup(
-    name = 'data-pack',
-    packages = ['datapack'],
-    version = '0.0',
-    description = 'Data packages',
-    author = 'Stéfan van der Walt',
-    author_email = 'stefanv@berkeley.edu',
-    url = 'https://github.com/data-pack/data-pack',
-    download_url = 'https://github.com/data-pack/data-pack/archive/0.0.tar.gz',
-    keywords = ['data', 'packaging', 'science'],
-    classifiers = [],
+    name='data-pack',
+    packages=['datapack'],
+    version='0.0',
+    description='Data packages',
+    author='Stéfan van der Walt',
+    author_email='stefanv@berkeley.edu',
+    url='https://github.com/data-pack/data-pack',
+    download_url='https://github.com/data-pack/data-pack/archive/0.0.tar.gz',
+    keywords=['data', 'packaging', 'science'],
+    classifiers=[],
+    install_requires=[
+        "requests"
+    ],
+    setup_requires=[
+        "pytest-runner"
+    ],
+    tests_require=[
+        "pytest"
+    ]
 )
